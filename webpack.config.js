e984715bc770d5c483933e764b10a580e05baba4
aeh1707d <abdelhadi.hireche@outlook.com>
2022-07-06 14:15:56 +0100

    Fix linter error

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin'); /* eslint-disable-line no-alert */
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // eslint-disable-line
 
 module.exports = {
   mode: 'development',
